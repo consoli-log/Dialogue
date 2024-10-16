a8769a17b930e1bbdec2a66447ab5fd313aaea31
consoli.log <eumsoli@gmail.com>
2024-10-16 15:58:33 +0900

    🎉 Dialogue 프로젝트 시작!

---
+++ src/main/java/com/soli/dialogue/DialogueApplication.java
@@ -0,0 +1,13 @@
+package com.soli.dialogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialogueApplication.class, args);
+	}
+
+}
